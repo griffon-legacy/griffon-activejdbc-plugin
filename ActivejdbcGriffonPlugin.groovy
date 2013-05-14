f3229175a38e273cfb1198010cee0929ad38f49d
Andres Almiray <aalmiray@yahoo.com>
2013-05-14 15:44:17 -0500

    JDK6 Compatibility

--- ActivejdbcGriffonPlugin.groovy
+++ ActivejdbcGriffonPlugin.groovy
@@ -18,7 +18,7 @@
  */
 class ActivejdbcGriffonPlugin {
     // the plugin version
-    String version = '1.2.0'
+    String version = '1.2.1'
     // the version or versions of Griffon the plugin is designed for
     String griffonVersion = '1.3.0 > *'
     // the other plugins this plugin depends on
